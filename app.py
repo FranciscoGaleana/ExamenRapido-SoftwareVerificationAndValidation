b36edffb8dd6a9fde9f075602fa8f086cfcf3382
FranciscoGaleana <168003814+FranciscoGaleana@users.noreply.github.com>
2025-02-09 15:01:50 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ import logging
 #Initializing flask framework
 app = Flask(__name__)
 
-#Function for getting de current date (Year,Month,Day)
+#Initializing logger, creating a .txt file for logs, setting level as INFO
+#and setting format as date, levelname, and logmessage
+logging.basicConfig(filename="api_log.txt", level=logging.INFO, 
+format='%(asctime)s - %(levelname)s - %(message)s')
+
+#Function for getting the current date (Year,Month,Day)
 def get_date():
     return date.today()
 
@@ -18,23 +23,30 @@ def transaction_id():
 #Initial function to test a simple GET request
 @app.route("/", methods=['GET'])
 def inicio():
-    return jsonify(
-        {"message": "API working corrrectly",
-         "method" : f"{request.method}",
-         "date": f"{get_date()}",
-         "http-status": 200,
-         "reques-id": f"{transaction_id()}"
-         })
+    response = {"endpoint": f"{request.path}",
+                "message": "API working corrrectly",
+                 "method" : f"{request.method}",
+                 "date": f"{get_date()}",
+                 "http-status": 200,
+                 "reques-id": f"{transaction_id()}"
+                }
+    
+    logging.info(response)
+    return jsonify(response)
 
 #Function for listing business general information
 @app.route("/business/info", methods=['GET'])
 def business_info():
-    return jsonify({"message": "Tech Products",
-                    "method" : f"{request.method}",
-                    "date": f"{get_date()}",
-                    "http-status": 200,
-                    "request-id": f"{transaction_id()}"
-                    })
+    response = {"endpoint": f"{request.path}",
+                "message": "Tech Products",
+                "method" : f"{request.method}",
+                "date": f"{get_date()}",
+                "http-status": 200,
+                "request-id": f"{transaction_id()}"
+                }
+    
+    logging.info(response)
+    return jsonify(response)
 
 products = [
     {"id": 1, "ProductName": "Computers"},
@@ -54,34 +66,57 @@ products = [
 def business_products():
     #Handling request, if GET: only return product info
     if request.method == 'GET':
-        return jsonify({"products": products,
-                        "method" : f"{request.method}",
-                        "date": f"{get_date()}",
-                        "http-status": 200,
-                        "request-id": f"{transaction_id()}"
-                    })
+        response = {"endpoint":f"{request.path}",
+                    "products": products,
+                    "method" : f"{request.method}",
+                    "date": f"{get_date()}",
+                    "http-status": 200,
+                    "request-id": f"{transaction_id()}"
+                    }
+        
+        logging.info(response)
+        return jsonify(response)
 
     #If request type is POST then verify if product already exists, if not
     #add product to the existing list of products
     elif request.method == 'POST':
         #Reading the entered product
         data = request.get_json()
-        product_name = data.get("ProductName")
+        product_name = data.get("ProductName", None)
+
+        if not product_name:
+            response = {"endpoint": f"{request.path}",
+                        "message": "Product name is required. Enter: \"ProdutName\": \"Value\"",
+                        "method": f"{request.method}",
+                        "date": f"{get_date()}",
+                        "http-status": 400,
+                        "request-id": f"{transaction_id()}",}
+            
+            logging.info(response)
+            return jsonify(response)
+        
 
         #Verifying if the product already exists in the original list
         #If so, do not append it twice
         for product in products:
             if product["ProductName"].lower() == product_name.lower():
-                return jsonify({
-                    "message": "Product already exists",
-                    "method": f"{request.method}",
-                    "date": f"{get_date()}",
-                    "http-status": 400,
-                    "request-id": f"{transaction_id()}",
-                })
+                response = {"endpoint": f"{request.path}",
+                            "message": "Product already exists",
+                            "method": f"{request.method}",
+                            "date": f"{get_date()}",
+                            "http-status": 400,
+                            "request-id": f"{transaction_id()}",
+                            }
+                
+                logging.info(response)
+                return jsonify(response)
         
         #Calculating the ID for the new product
-        new_id = max(product["id"] for product in products) + 1
+        if products:
+            new_id = products[-1]["id"] + 1
+        
+        else:
+            new_id = 1
 
         #Creating new product
         new_product = {"id": new_id, "ProductName": product_name}
@@ -89,14 +124,17 @@ def business_products():
         #Adding new product to the existing products list
         products.append(new_product)
 
-        return jsonify({
-            "message": "Product added successfully",
-            "new_product": new_product,
-            "method": f"{request.method}",
-            "date": f"{get_date()}",
-            "http-status": 201,
-            "request-id": f"{transaction_id()}"
-        })
+        response = {"endpoint": f"{request.path}",
+                    "message": "Product added successfully",
+                    "new_product": new_product,
+                    "method": f"{request.method}",
+                    "date": f"{get_date()}",
+                    "http-status": 201,
+                    "request-id": f"{transaction_id()}"
+                    }
+        
+        logging.info(response)
+        return jsonify(response)
 
     #If the request wasnt GET or POST return an error message
     """elif request.method not in ["GET", "POST"]:
@@ -105,13 +143,16 @@ def business_products():
 #Function for listing business contact info
 @app.route("/business/contact", methods=['GET'])
 def contact():
-    return jsonify({
-            "message": [{"TelephoneNumber": 123456789}, {"email": "test@gmail.com"}],
-            "method": f"{request.method}",
-            "date": f"{get_date()}",
-            "http-status": 201,
-            "request-id": f"{transaction_id()}"
-        })
+    response = {"endpoint": f"{request.path}",
+                "message": [{"TelephoneNumber": 123456789}, {"email": "test@gmail.com"}],
+                "method": f"{request.method}",
+                "date": f"{get_date()}",
+                "http-status": 201,
+                "request-id": f"{transaction_id()}"
+                }
+    
+    logging.info(response)
+    return jsonify(response)
 
 #Keeping the API running until manually stop
 if __name__ == '__main__':
